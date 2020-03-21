8227f0a66ca6c3a13249264af48b938a6aba1d6d
Melissa Murphey <melissamurphey77@gmail.com>
2020-03-21 18:02:12 -0500

    Complete additions Pt.2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -4,27 +4,37 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Skill extends AbstractEntity {
     //extending AbstractEntity inherits id and name automatically
 
+    //these adds skill and description to the id and name we already have to Skill
     private String skill;
-    //this adds skill and description to the id and name we already have to Skill
+
+    @Size(max = 500, message = "Description can't be more than 500 characters.")
+    private String description;
+
 
     //no-arg constructor
     public Skill(){
     }
 
     //validation
-    public Skill(String skill){
+    public Skill(String skill, String description){
         this.skill = skill;
+        this.description = description;
     }
 
 
     public String getSkill() { return skill; }
 
     public void setSkill(String skill) { this.skill = skill; }
+
+    public String getDescription() { return description;}
+
+    public void setDescription(String description) { this.description = description;}
 
 
 }
