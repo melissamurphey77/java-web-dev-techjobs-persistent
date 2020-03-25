87039971b39a245dfa8b1b69fd8d0fa2af48e56b
Melissa Murphey <melissamurphey77@gmail.com>
2020-03-25 13:48:35 -0500

    I think I'm done TBD

--- queries.sql
+++ queries.sql
@@ -1,11 +1,8 @@
 ## Part 1: Test it with SQL
 INSERT INTO job
-VALUES (200, "LaunchCode", "Web Developer", "Java,JavaScript");
+VALUES (200, "LaunchCode", "Web Developer");
 
 ## Part 2: Test it with SQL
---SELECT (location)
---FROM (job, `employer`);
---WHERE (`location` = St.Louis);
 
 ## Part 3: Test it with SQL
 
