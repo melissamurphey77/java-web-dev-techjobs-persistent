8fd928a608f7ec61220d104e5f7275b55ecaeaef
Melissa Murphey <melissamurphey77@gmail.com>
2020-03-25 16:15:18 -0500

    added sql queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,18 @@
 ## Part 1: Test it with SQL
-INSERT INTO job
-VALUES (200, "LaunchCode", "Web Developer");
+
+SHOW COLUMNS FROM job;
 
 ## Part 2: Test it with SQL
 
+SELECT * FROM employer WHERE location="St. Louis";
+
+
 ## Part 3: Test it with SQL
 
+DROP TABLE job;
+
+
 ## Part 4: Test it with SQL
+SELECT skill.name, skill.description
+FROM skill
+ORDER BY skill.name
